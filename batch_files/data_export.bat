fe1ea184ecab5955724aeeb8275961655383a7cd
adacaleb <hatajc@gmail.com>
2024-04-11 12:04:51 -0500

    proper setup to run seperate dev/prod apps now

--- batch_files/data_export.bat
+++ batch_files/data_export.bat
@@ -1,6 +1,6 @@
-sqlcmd -S GEARSERVER\SQLEXPRESS -d PRODUCTION -E -Q "SELECT [Job] FROM [PRODUCTION].[dbo].[Material_Req] WHERE Last_Updated > DATEADD(SECOND, -5,GETDATE())" -o "c:/phoenixapps/shophawk/csv_files/material.csv" -W -w 1024 -s "`" -f 65001 -h -1 
-sqlcmd -S GEARSERVER\SQLEXPRESS -d PRODUCTION -E -Q "SELECT [Job] FROM [PRODUCTION].[dbo].[Job] WHERE Last_Updated > DATEADD(SECOND, -5,GETDATE())" -o "c:/phoenixapps/shophawk/csv_files/jobs.csv" -W -w 1024 -s "`" -f 65001 -h -1 
-sqlcmd -S GEARSERVER\SQLEXPRESS -d PRODUCTION -E -Q "SELECT [Job] FROM [PRODUCTION].[dbo].[Job_Operation] WHERE Last_Updated > DATEADD(SECOND,-5,GETDATE())" -o "c:/phoenixapps/shophawk/csv_files/runlistops.csv" -W -w 1024 -s "`" -f 65001 -h -1
+sqlcmd -S GEARSERVER\SQLEXPRESS -d PRODUCTION -E -Q "SELECT [Job] FROM [PRODUCTION].[dbo].[Material_Req] WHERE Last_Updated > DATEADD(SECOND, -6,GETDATE())" -o "c:/phoenixapps/shophawk_2.0/csv_files/material.csv" -W -w 1024 -s "`" -f 65001 -h -1 
+sqlcmd -S GEARSERVER\SQLEXPRESS -d PRODUCTION -E -Q "SELECT [Job] FROM [PRODUCTION].[dbo].[Job] WHERE Last_Updated > DATEADD(SECOND, -6,GETDATE())" -o "c:/phoenixapps/shophawk_2.0/csv_files/jobs.csv" -W -w 1024 -s "`" -f 65001 -h -1 
+sqlcmd -S GEARSERVER\SQLEXPRESS -d PRODUCTION -E -Q "SELECT [Job] FROM [PRODUCTION].[dbo].[Job_Operation] WHERE Last_Updated > DATEADD(SECOND,-6,GETDATE())" -o "c:/phoenixapps/shophawk_2.0/csv_files/runlistops.csv" -W -w 1024 -s "`" -f 65001 -h -1
 
 
 
--- config/runtime.exs
+++ config/runtime.exs
@@ -17,8 +17,10 @@ import Config
 # Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
 # script that automatically sets the env var above.
 
-# Configure your database
-  config :shophawk, Shophawk.Repo,
+  env = System.get_env("MIX_INV")
+  if env == "prod" do
+    # Configure your database
+    config :shophawk, Shophawk.Repo,
     username: "postgres",
     password: "Gear1234",
     hostname: "localhost",
@@ -27,34 +29,34 @@ import Config
     show_sensitive_data_on_connection_error: true,
     pool_size: 10
 
-  # The secret key base is used to sign/encrypt cookies and other secrets.
-  # A default value is used in config/dev.exs and config/test.exs but you
-  # want to use a different value for prod and you most likely don't want
-  # to check this value into version control, so we use an environment
-  # variable instead.
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
+      # The secret key base is used to sign/encrypt cookies and other secrets.
+      # A default value is used in config/dev.exs and config/test.exs but you
+      # want to use a different value for prod and you most likely don't want
+      # to check this value into version control, so we use an environment
+      # variable instead.
+    secret_key_base =
+      System.get_env("SECRET_KEY_BASE") ||
+        raise """
+        environment variable SECRET_KEY_BASE is missing.
+        You can generate one by calling: mix phx.gen.secret
+        """
 
-  host = System.get_env("PHX_HOST") || "127.0.0.1"
-  port = String.to_integer(System.get_env("PORT") || "4001")
+    host = System.get_env("PHX_HOST") || "127.0.0.1"
+    port = String.to_integer(System.get_env("PORT") || "4001")
 
-  config :shophawk, ShophawkWeb.Endpoint,
-    check_origin: ["http://192.168.2.73:4001"],
-    url: [host: host, port: 443, scheme: "https"],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0},
-      port: port
-    ],
-    secret_key_base: secret_key_base
-
+    config :shophawk, ShophawkWeb.Endpoint,
+      check_origin: ["http://192.168.2.73:4001"],
+      url: [host: host, port: 443, scheme: "https"],
+      http: [
+        # Enable IPv6 and bind on all interfaces.
+        # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+        # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+        # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+        ip: {0, 0, 0, 0},
+        port: port
+      ],
+      secret_key_base: secret_key_base
+    end
   # ## SSL Support
   #
   # To get SSL working, you will need to add the `https` key
